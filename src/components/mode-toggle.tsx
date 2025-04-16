f76914dc7d82949681a6a061759d9766edfe0b9d
Andrew Hoang <hoangaatrox23504@gmail.com>
2025-04-16 11:12:07 +0700

    feat: update icon fill when path active, styled css sidebar

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import * as React from 'react'
-import { Laptop, Moon, Sun } from 'lucide-react'
+import { Devices, Moon, Sun } from '@phosphor-icons/react'
 import { useTheme } from 'next-themes'
 
 import { Button } from '@/components/ui/button'
@@ -18,30 +18,38 @@ export function ModeToggle() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
+        <button className="inline-flex transition-all border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50 rounded-full p-3 font-bold outline-0 cursor-pointer">
+          <Sun
+            size={18}
+            weight="bold"
+            className="rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+          />
+          <Moon
+            size={18}
+            weight="bold"
+            className="absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+          />
+        </button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
+      <DropdownMenuContent align="end" side="right" className="p-1 rounded-2xl">
         <DropdownMenuItem
           onClick={() => setTheme('light')}
-          className="cursor-pointer"
+          className="cursor-pointer py-2.5 rounded-[12px] text-[15px]"
         >
           <Sun /> Light
         </DropdownMenuItem>
         <DropdownMenuItem
           onClick={() => setTheme('dark')}
-          className="cursor-pointer"
+          className="cursor-pointer py-2.5 rounded-[12px] text-[15px]"
         >
-          <Moon /> Dark
+          <Moon />
+          Dark
         </DropdownMenuItem>
         <DropdownMenuItem
           onClick={() => setTheme('system')}
-          className="cursor-pointer"
+          className="cursor-pointer py-2.5 rounded-[12px] text-[15px]"
         >
-          <Laptop /> System
+          <Devices /> System
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
